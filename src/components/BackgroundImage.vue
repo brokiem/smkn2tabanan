f8a4067699331ff798ad75cfdff86bef217b518d
rikapuspita01 <rikarpl21@smkn2tabanan.sch.id>
2022-12-02 07:22:50 +0800

    Use position relative and absolute for info card

--- src/components/BackgroundImage.vue
+++ src/components/BackgroundImage.vue
@@ -1,10 +1,18 @@
 <template>
-  <img class="w-full h-[105vh] object-cover" src="/public/images/atas-blur-min.jpg" alt="Atas SMK DUTA">
+  <div class="relative">
+    <img class="w-full h-[105vh] object-cover" src="/public/images/atas-blur-min.jpg" alt="Atas SMK DUTA">
+    <InfoCard class="absolute"/>
+  </div>
 </template>
 
 <script>
+import InfoCard from "@/components/InfoCard.vue";
+
 export default {
-  name: "LandingImage"
+  name: "LandingImage",
+  components: {
+    InfoCard
+  }
 }
 </script>
 
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -3,7 +3,6 @@
   <Navbar/>
   <!-- Landing image + info card -->
   <BackgroundImage/>
-  <InfoCard />
   <!-- Kompetensi section-->
   <Kompetensi />
   <!-- Article section -->
@@ -22,7 +21,7 @@ import Article from "@/components/Article.vue";
 
 export default {
   components: {
-    BackgroundImage, InfoCard, Kompetensi, Article
+    BackgroundImage, Kompetensi, Article
   }
 }
 </script>
