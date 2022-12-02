1a1b796a4fffdc9c510a255689cd5a68c0b2cacd
brokiem <milyhqvideo@gmail.com>
2022-12-02 18:13:47 +0800

    use WebHashHistory for routing for noW (github pages limitation)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 import MainView from "../views/MainView.vue";
 import FasilitasSekolahView from "../views/FasilitasSekolahView.vue";
 import SejarahSekolahView from "../views/SejarahSekolahView.vue";
@@ -14,7 +14,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: routes
 });
 
