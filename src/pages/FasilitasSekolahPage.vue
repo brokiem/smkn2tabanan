f07c7fc073af9556915a36d1ee3ee101618680de
brokiem <milyhqvideo@gmail.com>
2022-12-02 17:19:34 +0800

    Some adjustments to FasilitasSekolahPage.vue

--- src/pages/FasilitasSekolahPage.vue
+++ src/pages/FasilitasSekolahPage.vue
@@ -6,11 +6,11 @@
     Fasilitas Sekolah
   </h2>
 
-  <div class="container mx-auto px-4">
+  <div class="container mx-auto px-4 md:max-w-screen-lg pt-6">
     <!-- Page card title -->
     <div class="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-      <h5 class="mb-1 text-2xl font-medium tracking-tight text-gray-700 dark:text-white">
-        BENGKEL KOMPUTER
+      <h5 class="mb-2 text-2xl font-base tracking-tight text-gray-700 dark:text-white">
+        Bengkel Komputer
       </h5>
       <p class="mt-1 text-base font-normal text-gray-700 dark:text-gray-400">
         SMKN 2 Tabanan  memiliki 3 bengkel komputer utama lengkap dengan LAN beserta jaringan internet.
@@ -20,20 +20,20 @@
     </div>
     <!-- Page card title -->
     <div class="mt-5 block w-full p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-      <h5 class="mb-1 text-2xl font-medium tracking-tight text-gray-700 dark:text-white">
-        STUDIO BROADCAST
+      <h5 class="mb-2 text-2xl font-base tracking-tight text-gray-700 dark:text-white">
+        Studio Broadcast
       </h5>
-      <p class="mt-1 text-base font-normal text-gray-700 dark:text-gray-400">
+      <p class="mt-1 text-base font-base text-gray-700 dark:text-gray-400">
         Jurusan Multimedia disediakan studio broadcast untuk melaksanakan siaran baik secara online maupun offline,
         dengan didukung peralatan yang profesional seperti tersedianya video mixer, audio mixer dan tata cahaya.
       </p>
     </div>
     <!-- Page card title -->
     <div class="mt-5 block w-full p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-      <h5 class="mb-1 text-2xl font-medium tracking-tight text-gray-700 dark:text-white">
-        STUDIO PHOTO
+      <h5 class="mb-1 text-2xl font-base tracking-tight text-gray-700 dark:text-white">
+        Studio Photo
       </h5>
-      <p class="mt-1 text-base font-normal text-gray-700 dark:text-gray-400">
+      <p class="mt-2 text-base font-normal text-gray-700 dark:text-gray-400">
         Untuk Kompetensi Keahlian Multimedia disediakan juga ruangan studio photo yang digunakan untuk praktek
         pengolahan gambar digital yang dilengkapi dengan camera DSLR beserta tata cahaya dengan background
         yang bisa diganti-ganti.
@@ -41,10 +41,10 @@
     </div>
     <!-- Page card title -->
     <div class="mt-5 block w-full p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-      <h5 class="mb-1 text-2xl font-medium tracking-tight text-gray-700 dark:text-white">
-        DAPUR DAN RUANG SAJI
+      <h5 class="mb-1 text-2xl font-base tracking-tight text-gray-700 dark:text-white">
+        Dapur dan Ruang Saji
       </h5>
-      <p class="mt-1 text-base font-normal text-gray-700 dark:text-gray-400">
+      <p class="mt-2 text-base font-normal text-gray-700 dark:text-gray-400">
         Kompetensi Keahlian Tata Boga disediakan dapur yang dilengkapi dengan alat-alat sesuai standar
         sehingga dalam pengolahan makanan dan minuman dapat dilakukan dengan mudah. Selain itu pada
         kompetensi keahlian ini juga dilengkapi dengan ruangan saji yang digunakan sebagai wadah
