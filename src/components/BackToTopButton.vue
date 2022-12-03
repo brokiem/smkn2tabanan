59555bee3900388fedb17224beb7449f3ae7fa7e
brokiem <milyhqvideo@gmail.com>
2022-12-03 16:39:06 +0800

    Smaller back to top button

--- src/components/BackToTopButton.vue
+++ src/components/BackToTopButton.vue
@@ -1,6 +1,6 @@
 <template>
-  <button @click="scrollToTop" id="top-button" class="fade-in fixed z-50 hidden p-3 bg-gray-100 hover:bg-gray-200 border-2 border-light-blue/50 rounded-md shadow-md transition duration-100 bottom-[30px] right-[30px]">
-    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <button @click="scrollToTop" id="top-button" class="fade-in fixed z-50 hidden p-2 bg-gray-100 hover:bg-gray-200 border-2 border-light-blue/50 rounded-md shadow-md transition duration-100 bottom-[30px] right-[30px]">
+    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
     </svg>
   </button>
