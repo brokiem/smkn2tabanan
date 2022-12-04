ec2337d6dd3f9feeda65a78eb20d13efa4bc6945
brokiem <milyhqvideo@gmail.com>
2022-12-04 12:42:28 +0800

    Preload images in router

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,16 +15,67 @@ function lazyLoad(view: string) {
     return () => import(`@/views/${view}.vue`);
 }
 
+function preloadImages(urls: Array<string>) {
+    urls.forEach(url => {
+        const img = new Image();
+        img.src = url;
+    });
+}
+
 const routes = [
-    {path: "/", name: "main", component: lazyLoad('MainView')},
-    {path: "/profil/fasilitas", name: "fasilitas-sekolah", component: lazyLoad('FasilitasSekolahView')},
-    {path: "/profil/sejarah", name: "sejarah-sekolah", component: lazyLoad('SejarahSekolahView')},
-    {path: "/profil/visi-misi", name: "visi-misi", component: lazyLoad('VisiMisiView')},
-    {path: "/profil/logo", name: "logo", component: lazyLoad('LogoSekolahView')},
-    {path: "/profil/struktur-organisasi", name: "struktur-organisasi", component: lazyLoad('StrukturOrganisasiView')},
-    {path: "/kompetensi-keahlian", name: "kompetensi-keahlian", component: lazyLoad('KompetensiKeahlianView')},
-    {path: "/jurusan", redirect: '/kompetensi-keahlian'},
-    {path: "/kesiswaan/ekstrakulikuler", name: "ekstrakulikuler", component: lazyLoad('EkstrakulikulerView')}
+    {
+        path: "/",
+        name: "main",
+        component: lazyLoad('MainView'),
+        beforeEnter: () => preloadImages(['/public/images/atas-blur-min.webp'])
+    },
+    {
+        path: "/profil/fasilitas",
+        name: "fasilitas-sekolah",
+        component: lazyLoad('FasilitasSekolahView')
+    },
+    {
+        path: "/profil/sejarah",
+        name: "sejarah-sekolah",
+        component: lazyLoad('SejarahSekolahView')
+    },
+    {
+        path: "/profil/visi-misi",
+        name: "visi-misi",
+        component: lazyLoad('VisiMisiView')
+    },
+    {
+        path: "/profil/logo",
+        name: "logo",
+        component: lazyLoad('LogoSekolahView'),
+        beforeEnter: () => preloadImages(['/public/images/logo-smk2.png'])
+    },
+    {
+        path: "/profil/struktur-organisasi",
+        name: "struktur-organisasi",
+        component: lazyLoad('StrukturOrganisasiView'),
+        beforeEnter: () => preloadImages(['/public/images/struktur-organisasi.png'])
+    },
+    {
+        path: "/kompetensi-keahlian",
+        name: "kompetensi-keahlian",
+        component: lazyLoad('KompetensiKeahlianView'),
+        beforeEnter: () => preloadImages([
+            '/public/images/coding-rpl.jpg',
+            '/public/images/camera-mm.jpg',
+            '/public/images/food-boga.jpg',
+            '/public/images/calc-akl.jpg'
+        ])
+    },
+    {
+        path: "/jurusan",
+        redirect: '/kompetensi-keahlian'
+    },
+    {
+        path: "/kesiswaan/ekstrakulikuler",
+        name: "ekstrakulikuler",
+        component: lazyLoad('EkstrakulikulerView')
+    }
 ];
 
 const router = createRouter({
