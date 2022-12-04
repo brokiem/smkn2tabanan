3303d20ddb34be8a0b1d64f04dbfa450201dd961
brokiem <milyhqvideo@gmail.com>
2022-12-04 09:00:01 +0800

    Fix build failed

--- src/pages/StrukturOriganisasiPage.vue
+++ src/pages/StrukturOriganisasiPage.vue
@@ -9,7 +9,7 @@
 
   <div class="container mx-auto px-4 md:max-w-screen-lg">
     <div class="flex justify-center">
-      <img src="public/images/struktur-organisasi.png" width="862" height="644" alt="Struktur organisasi">
+      <img src="/public/images/struktur-organisasi.png" width="862" height="644" alt="Struktur organisasi">
     </div>
   </div>
 
