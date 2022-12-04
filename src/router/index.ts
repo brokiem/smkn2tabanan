2841cfd29e8f907f72f613357a489bded6548cfe
brokiem <milyhqvideo@gmail.com>
2022-12-04 10:49:07 +0800

    Try vue router lazy load

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,19 @@
 import {createRouter, createWebHashHistory} from "vue-router";
-import MainView from "../views/MainView.vue";
-import FasilitasSekolahView from "../views/FasilitasSekolahView.vue";
-import SejarahSekolahView from "../views/SejarahSekolahView.vue";
-import VisiMisiView from "../views/VisiMisiView.vue";
-import KompetensiKeahlianView from "../views/KompetensiKeahlianView.vue";
-import EkstrakulikulerView from "../views/EkstrakulikulerView.vue";
-import LogoSekolahView from "../views/LogoSekolahView.vue";
-import StrukturOrganisasiView from "../views/StrukturOrganisasiView.vue";
+
+function lazyLoad(view){
+    return() => import(`@/views/${view}.vue`)
+}
 
 const routes = [
-    {path: "/", name: "main", component: MainView},
-    {path: "/profil/fasilitas", name: "fasilitas-sekolah", component: FasilitasSekolahView},
-    {path: "/profil/sejarah", name: "sejarah-sekolah", component: SejarahSekolahView},
-    {path: "/profil/visi-misi", name: "visi-misi", component: VisiMisiView},
-    {path: "/profil/logo", name: "logo", component: LogoSekolahView},
-    {path: "/profil/struktur-organisasi", name: "struktur-organisasi", component: StrukturOrganisasiView},
-    {path: "/kompetensi-keahlian", name: "kompetensi-keahlian", component: KompetensiKeahlianView},
+    {path: "/", name: "main", component: lazyLoad('MainView')},
+    {path: "/profil/fasilitas", name: "fasilitas-sekolah", component: lazyLoad('FasilitasSekolahView')},
+    {path: "/profil/sejarah", name: "sejarah-sekolah", component: lazyLoad('SejarahSekolahView')},
+    {path: "/profil/visi-misi", name: "visi-misi", component: lazyLoad('VisiMisiView')},
+    {path: "/profil/logo", name: "logo", component: lazyLoad('LogoSekolahView')},
+    {path: "/profil/struktur-organisasi", name: "struktur-organisasi", component: lazyLoad('StrukturOrganisasiView')},
+    {path: "/kompetensi-keahlian", name: "kompetensi-keahlian", component: lazyLoad('KompetensiKeahlianView')},
     {path: "/jurusan", redirect: '/kompetensi-keahlian'},
-    {path: "/kesiswaan/ekstrakulikuler", name: "ekstrakulikuler", component: EkstrakulikulerView}
+    {path: "/kesiswaan/ekstrakulikuler", name: "ekstrakulikuler", component: lazyLoad('EkstrakulikulerView')}
 ];
 
 const router = createRouter({
