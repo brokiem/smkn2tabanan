d01fd731d7ce0f64fb0cc576f246798e8f1c82ab
brokiem <milyhqvideo@gmail.com>
2022-12-04 13:14:06 +0800

    Working image preload

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,19 +15,34 @@ function lazyLoad(view: string) {
     return () => import(`@/views/${view}.vue`);
 }
 
-function preloadImages(urls: Array<string>) {
-    urls.forEach(url => {
-        const img = new Image();
-        img.src = url;
+function preloadImage(url: string) {
+    const img = new Image();
+    img.src = url;
+
+    return new Promise(resolve => {
+        const image = new Image();
+        image.addEventListener('load', () => {
+            resolve(image);
+        });
+        image.src = url;
     });
+}
+
+function preloadImages(urls: Array<string>) {
+    let promises: any[] = [];
+
+    urls.forEach(url => {
+        promises.push(preloadImage(url));
+    });
+
+    return promises;
 }
 
 const routes = [
     {
         path: "/",
         name: "main",
-        component: lazyLoad('MainView'),
-        beforeEnter: () => preloadImages(['/smkn2tabanan/images/atas-blur-min.webp'])
+        component: lazyLoad('MainView')
     },
     {
         path: "/profil/fasilitas",
@@ -47,25 +62,17 @@ const routes = [
     {
         path: "/profil/logo",
         name: "logo",
-        component: lazyLoad('LogoSekolahView'),
-        beforeEnter: () => preloadImages(['/smkn2tabanan/images/logo-smk2.png'])
+        component: lazyLoad('LogoSekolahView')
     },
     {
         path: "/profil/struktur-organisasi",
         name: "struktur-organisasi",
-        component: lazyLoad('StrukturOrganisasiView'),
-        beforeEnter: () => preloadImages(['/smkn2tabanan/images/struktur-organisasi.png'])
+        component: lazyLoad('StrukturOrganisasiView')
     },
     {
         path: "/kompetensi-keahlian",
         name: "kompetensi-keahlian",
-        component: lazyLoad('KompetensiKeahlianView'),
-        beforeEnter: () => preloadImages([
-            '/smkn2tabanan/images/coding-rpl.jpg',
-            '/smkn2tabanan/images/camera-mm.jpg',
-            '/smkn2tabanan/images/food-boga.jpg',
-            '/smkn2tabanan/images/calc-akl.jpg'
-        ])
+        component: lazyLoad('KompetensiKeahlianView')
     },
     {
         path: "/jurusan",
@@ -87,9 +94,35 @@ const router = createRouter({
     routes: routes
 });
 
-router.beforeEach(() => {
+router.beforeEach((to, from, next) => {
     // Start the route progress bar.
     NProgress.start()
+
+    let promises = [];
+
+    switch (to.name) {
+        case "main":
+            promises = preloadImages(['/smkn2tabanan/images/atas-blur-min.webp'])
+            break;
+        case "logo":
+            promises = preloadImages(['/smkn2tabanan/images/logo-smk2.png'])
+            break;
+        case "struktur-organisasi":
+            promises = preloadImages(['/smkn2tabanan/images/struktur-organisasi.png'])
+            break;
+        case "kompetensi-keahlian":
+            promises = preloadImages([
+                '/smkn2tabanan/images/coding-rpl.jpg',
+                '/smkn2tabanan/images/camera-mm.jpg',
+                '/smkn2tabanan/images/food-boga.jpg',
+                '/smkn2tabanan/images/calc-akl.jpg'
+            ])
+            break;
+    }
+
+    Promise.all(promises).then(function () {
+        next();
+    });
 });
 
 router.afterEach(() => {
