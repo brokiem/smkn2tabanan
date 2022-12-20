aed480e7d41e78a25a358991913795deef447491
brokiem <milyhqvideo@gmail.com>
2022-12-20 22:28:40 +0800

    rest js update

--- public/assets/rest.js
+++ public/assets/rest.js
@@ -1,30 +1,30 @@
 const CONSTANTS = {
-    API_URL: 'https://exuberant-toga-wasp.cyclic.app',
+    API_URL: 'https://exuberant-toga-wasp.cyclic.app/v1',
 }
 
 export function fetchNews(callback) {
-    return fetch(`${CONSTANTS.API_URL}/v1/berita/all`)
+    return fetch(`${CONSTANTS.API_URL}/berita/all`)
         .then(res => res.json())
         .then(callback)
         .catch(error => console.log('error', error));
 }
 
 export function fetchAnnouncements(callback) {
-    return fetch(`${CONSTANTS.API_URL}/v1/pengumuman/all`)
+    return fetch(`${CONSTANTS.API_URL}/pengumuman/all`)
         .then(res => res.json())
         .then(callback)
         .catch(error => console.log('error', error));
 }
 
 export function getNewsDetail(id, callback) {
-    return fetch(`${CONSTANTS.API_URL}/v1/berita/get/${id}`)
+    return fetch(`${CONSTANTS.API_URL}/berita/get/${id}`)
         .then(res => res.json())
         .then(callback)
         .catch(error => console.log('error', error));
 }
 
 export function getAnnouncementDetail(id, callback) {
-    return fetch(`${CONSTANTS.API_URL}/v1/pengumuman/get/${id}`)
+    return fetch(`${CONSTANTS.API_URL}/pengumuman/get/${id}`)
         .then(res => res.json())
         .then(callback)
         .catch(error => console.log('error', error));
