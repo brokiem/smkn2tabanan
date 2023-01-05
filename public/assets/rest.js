6ce2c8ca5fc2ede2443cb7b848331344d45d9493
brokiem <milyhqvideo@gmail.com>
2023-01-05 19:50:46 +0800

    rest.js update

--- public/assets/rest.js
+++ public/assets/rest.js
@@ -3,29 +3,129 @@ const CONSTANTS = {
 }
 
 export function fetchNews(callback) {
-    return fetch(`${CONSTANTS.API_URL}/v1/berita/all`)
+    fetch(`${CONSTANTS.API_URL}/v1/berita/all`, {method: 'GET', redirect: 'follow'})
         .then(res => res.json())
         .then(callback)
         .catch(error => console.log('error', error));
 }
 
 export function fetchAnnouncements(callback) {
-    return fetch(`${CONSTANTS.API_URL}/v1/pengumuman/all`)
+    fetch(`${CONSTANTS.API_URL}/v1/pengumuman/all`, {method: 'GET', redirect: 'follow'})
         .then(res => res.json())
         .then(callback)
         .catch(error => console.log('error', error));
 }
 
 export function getNewsDetail(id, callback) {
-    return fetch(`${CONSTANTS.API_URL}/v1/berita/get/${id}`)
+    fetch(`${CONSTANTS.API_URL}/v1/berita/get/${id}`, {method: 'GET', redirect: 'follow'})
         .then(res => res.json())
         .then(callback)
         .catch(error => console.log('error', error));
 }
 
 export function getAnnouncementDetail(id, callback) {
-    return fetch(`${CONSTANTS.API_URL}/v1/pengumuman/get/${id}`)
+    fetch(`${CONSTANTS.API_URL}/v1/pengumuman/get/${id}`, {method: 'GET', redirect: 'follow'})
         .then(res => res.json())
         .then(callback)
+        .catch(error => console.log('error', error));
+}
+
+export function publishNews(data, callback) {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("title", data.title);
+    urlencoded.append("image_header_url", data.image_header_url);
+    urlencoded.append("contents", data.contents);
+    urlencoded.append("posted_by", "Admin");
+    urlencoded.append("created_at", Date.now().toString());
+    urlencoded.append("updated_at", Date.now().toString());
+
+    const params = {
+        method: 'POST',
+        headers: headers,
+        body: urlencoded,
+        redirect: 'follow'
+    }
+
+    fetch(`${CONSTANTS.API_URL}/v1/berita/add`, params)
+        .then(callback)
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+export function publishAnnouncement(data, callback) {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("title", data.title);
+    urlencoded.append("image_header_url", data.image_header_url);
+    urlencoded.append("contents", data.contents);
+    urlencoded.append("posted_by", "Admin");
+    urlencoded.append("created_at", Date.now().toString());
+    urlencoded.append("updated_at", Date.now().toString());
+
+    const params = {
+        method: 'POST',
+        headers: headers,
+        body: urlencoded,
+        redirect: 'follow'
+    }
+
+    fetch(`${CONSTANTS.API_URL}/v1/pengumuman/add`, params)
+        .then(callback)
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+export function draftNews(data, callback) {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("title", data.title);
+    urlencoded.append("image_header_url", data.image_header_url);
+    urlencoded.append("contents", data.contents);
+    urlencoded.append("posted_by", "Admin");
+    urlencoded.append("created_at", Date.now().toString());
+    urlencoded.append("updated_at", Date.now().toString());
+
+    const params = {
+        method: 'POST',
+        headers: headers,
+        body: urlencoded,
+        redirect: 'follow'
+    }
+
+    fetch(`${CONSTANTS.API_URL}/v1/berita/draft`, params)
+        .then(callback)
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+export function draftAnnouncement(data, callback) {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("title", data.title);
+    urlencoded.append("image_header_url", data.image_header_url);
+    urlencoded.append("contents", data.contents);
+    urlencoded.append("posted_by", "Admin");
+    urlencoded.append("created_at", Date.now().toString());
+    urlencoded.append("updated_at", Date.now().toString());
+
+    const params = {
+        method: 'POST',
+        headers: headers,
+        body: urlencoded,
+        redirect: 'follow'
+    }
+
+    fetch(`${CONSTANTS.API_URL}/v1/pengumuman/draft`, params)
+        .then(callback)
+        .then(result => console.log(result))
         .catch(error => console.log('error', error));
 }
