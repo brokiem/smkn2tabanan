d512af5f746698ed28472952a2fbd4422993857c
brokiem <milyhqvideo@gmail.com>
2023-01-07 12:57:22 +0800

    Better skeleton shine animation

--- src/components/CardSkeleton.vue
+++ src/components/CardSkeleton.vue
@@ -2,21 +2,21 @@
   <div class="flex flex-col md:max-w-3xl text-gray-900 bg-white rounded-md border border-gray-300 shadow-sm transition duration-200">
     <!-- Card image skeleton -->
     <div class="relative">
-      <div class="rounded-t-md flex justify-center items-center w-full h-[189px] object-cover bg-gray-300 rounded dark:bg-gray-700">
-        <svg class="w-12 h-12 animate-pulse text-gray-200" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512"><path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"/></svg>
+      <div class="animate-shine rounded-t-md flex justify-center items-center w-full h-[189px] object-cover bg-gray-300 rounded dark:bg-gray-700">
+        <svg class="w-12 h-12 text-gray-100" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512"><path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"/></svg>
       </div>
     </div>
     <!-- Card contents skeleton -->
-    <div class="p-5 animate-pulse">
+    <div class="p-5">
       <!-- Card title skeleton -->
-      <div class="mt-1 mb-5 h-5 bg-gray-200 rounded-lg dark:bg-gray-700 max-w-[400px]"></div>
+      <div class="animate-shine mt-1 mb-5 h-5 bg-gray-200 rounded-lg dark:bg-gray-700 max-w-[400px]"></div>
       <!-- Card description skeleton -->
-      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-full lg:max-w-[380px] mb-2.5"></div>
-      <div class="h-2 lg:h-0 bg-gray-200 rounded-full dark:bg-gray-700 max-w-full lg:max-w-[380px] mb-2.5"></div>
-      <div class="h-0 lg:h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[320px] mb-2.5"></div>
-      <div class="h-0 lg:h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[240px] mb-2.5"></div>
+      <div class="animate-shine h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-full lg:max-w-[380px] mb-2.5"></div>
+      <div class="animate-shine h-2 lg:h-0 bg-gray-200 rounded-full dark:bg-gray-700 max-w-full lg:max-w-[380px] mb-2.5"></div>
+      <div class="animate-shine h-0 lg:h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[320px] mb-2.5"></div>
+      <div class="animate-shine h-0 lg:h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[240px] mb-2.5"></div>
       <!-- Card button skeleton -->
-      <div class="mt-6 h-8 bg-gray-200 rounded-md dark:bg-gray-700 w-full"></div>
+      <div class="animate-shine mt-6 h-8 bg-gray-200 rounded-md dark:bg-gray-700 w-full"></div>
     </div>
   </div>
 </template>
@@ -26,3 +26,17 @@ export default {
   name: "CardSkeleton"
 }
 </script>
+
+<style>
+.animate-shine {
+  background: linear-gradient(110deg, #D1D5DB 8%, #e3e3e3 18%, #D1D5DB 33%);
+  background-size: 200% 100%;
+  animation: 1.2s shine linear infinite;
+}
+
+@keyframes shine {
+  to {
+    background-position-x: -200%;
+  }
+}
+</style>
